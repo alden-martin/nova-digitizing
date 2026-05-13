063fbfd03144d663a18925cfb6c6368ba134b9e2
gazdarshariq0@gmail.com <aldenmartin.agwllc@gmail.com>
2026-05-13 21:46:38 +0500

    tawk to update

--- src/app/layout.js
+++ src/app/layout.js
@@ -35,15 +35,15 @@ export default function RootLayout({ children }) {
         {children}
         <Footer />
         <Script>{`
-          var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-          (function(){
-          var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-          s1.async=true;
-          s1.src='https://embed.tawk.to/693a74098a2e581980985f8d/1jc65ai07';
-          s1.charset='UTF-8';
-          s1.setAttribute('crossorigin','*');
-          s0.parentNode.insertBefore(s1,s0);
-          })();
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/6a04a49dd4796f1c35e061e0/1joh261tg';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
   
 `}</Script>
       </body>
